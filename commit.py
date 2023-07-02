a536342519d2a67a30b26fbe67b3ea61779e2f7d
Gunnar <gustavo.passarella@hotmail.com>
2023-07-02 19:51:18 +0100

    'testing'

--- commit.py
+++ commit.py
@@ -1,11 +1,20 @@
 import os
 import random
 
-for i in range(1, 5):
-    for _ in range(random.randint(0, 10)):
-        with open("test.txt", "a") as file:
-            file.write("test")
-        os.system("git add .")
-        os.system(f"git commit --date='2023-08-0{i} -m 'testing'")
+month = "07"
+start_day = 2
+end_day = 8
+
+for day in range(start_day, end_day+1):
+    if random.randint(0, 1) == 0:
+    
+        if day < 10:
+            day = f"0{day}"
+        
+        for _ in range(random.randint(0, 10)):
+            with open("test.txt", "a") as file:
+                file.write("test\n")
+            os.system("git add .")
+            os.system(f"git commit --date='2023-{month}-{day} -m 'testing'")
     
 os.system("git push")
