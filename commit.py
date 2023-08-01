a1f939e63204ac2ff158a049ecb0f424f4752f68
Gunnar <gustavo.passarella@hotmail.com>
2023-08-01 19:40:31 +0100

    'testing'

--- commit.py
+++ commit.py
@@ -2,9 +2,10 @@ import os
 import random
 
 for i in range(1, 5):
-    with open("test.txt", "a") as file:
-        file.write("test")
-    os.system("git add .")
-    os.system(f"git commit --date='2023-08-0{i} -m 'testing'")
+    for _ in range(random.randint(0, 10)):
+        with open("test.txt", "a") as file:
+            file.write("test")
+        os.system("git add .")
+        os.system(f"git commit --date='2023-08-0{i} -m 'testing'")
     
 os.system("git push")
